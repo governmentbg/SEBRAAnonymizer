3bade2a3d91037e202bafb6ddbd7408929688368
ismolenov1 <39150989+ismolenov1@users.noreply.github.com>
2022-07-21 09:19:00 +0300

    Update SEBRAAnonymizer.java

--- src/bg/egov/SEBRAAnonymizer.java
+++ src/bg/egov/SEBRAAnonymizer.java
@@ -8,6 +8,9 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -22,6 +25,7 @@ import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import com.ibm.icu.text.Transliterator;
@@ -37,15 +41,36 @@ public class SEBRAAnonymizer {
     private static final List<String> OTHER_PERSONAL_NAMES = Arrays.asList("ахмед", "ахмет", "мехмед", "сезгин", "мюмюн", "мустафа", "халил", "гюнер", "гюнай", 
             "сунай", "байрам", "ибрям", "хасан", "джейхан", "ерджан", "рамадан", "фатме", "редже", "хамид", "хамди", "юсеин", "хусейн", "сабри", " али", 
             "адам", "анави", "аврам", "арон", "финци", "шварц", " леви", "ицхак", "исак", "коен", "соломон", "яков", "щайн", "берг", 
-            "джераси", "езра", "фридман", "йосиф", "барух", "давид", "бехар");
+            "джераси", "езра", "фридман", "йосиф", "барух", "давид", "бехар", "илиева", "николова", "петрова", "иванова");
     
     private static final Transliterator TRANSLITERATOR = Transliterator.getInstance("Latin-Cyrillic");
     
     private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("d/M/yyyy");
     
-    private static final List<String> NON_PERSONAL_INDICATORS = Arrays.asList("мбал", "община", "ет ", "медицински", 
-            "оод", "сд ", "камара", "аптека", "агенция", " ад", "еад", "еод", "банка", "зк ", " съд", "университет", 
-            "асоциация", "зпк", " сие", "холдинг", "българск", "студентск"); 
+    private static final MessageDigest DIGEST;
+    static {
+        try {
+            DIGEST = MessageDigest.getInstance("SHA-256");
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+    
+    private static final List<String> NON_PERSONAL_INDICATORS = Arrays.asList("мбал", "община", "ет ", " ет", "медицински", 
+            "оод", "сд ", "камара", "аптека", "агенция", " ад", "еад", "еод", "дззд", "банка", "зк ", " съд", "университет", 
+            "асоциация", "фондация", "зпк", " сие", "холдинг", "българск", "европейск", "руски", "руска", "турск", "фонд", 
+            "сръбск", "македонск", "студентск", "румънск", "германск", "италианск", "арменск", "католическ", "протестантск", 
+            "мюсюлманск", "английск", "британск", "испанск", "френск", "норвежк", "бритиш", " клуб", "съвет", "общност", 
+            "вту ", "тп ", "тб ", "дсп ", "тпг ", "банк", " и ", "стз", "къща", "театър", "кооперация", "институт", "музей", "дружество",
+            "нуфи", "соу ", "оу ", "пг ", "пгее", "ипсмп", "апимп", "апмп", "аиппдм", " груп", "пги ", "дирекция", "верига", "стопанска",
+            "гимназия", "гпче", "гпие", "станция", "читалище", "пгехт", "пго", "пгтс", "пгим", "пгто", "пгпт", "пгс", "пгпо", "нгдек", "нгпи", 
+            "пгтхт", "пгдва", "спге", "сгсаг","пгметт", "пгххт", "пгхтд", "пгмт", "цнпх", "пгте", "пглв", "пгох", "ипазр", "патг", "пмг", 
+            "обединение", "ипжз", "филиал", " клон", "тпки", "комплекс", "продукт", "палата", "министерство", "ауто", " база", "секюрити", 
+            "библиотека", "област", "народна ", "сдружение", "общин", "сбдпл", "аиппмп", "лаборатор", "градина", "училище", "ппок", "амцсп", 
+            "филм ", "център", "ввму", "биогаз", "амбулатор", "агро ", "универс", "мбпл", "обединен", " комерс", "артс ", "компания", 
+            "прокуратура", "чси ", " унив", "комисия", "адвокат", "адв.", "хотел", " хоте", "оусв", "манастир", "консорциум", "ентърпр",
+            "импорт", "експорт", "импекс", " стил ", "селскостоп", "инженер", "мост ", "депозитар", "нотариус", "пето ", "изба ", 
+            "сбал", "мбал",  "аппмп", "асмп", "дмсгд", " дент", "енерджи", "airline", "експрес", "ресторант");
     
     private static final Pattern WHITESPACE = Pattern.compile("\\s+");
     
@@ -53,6 +78,16 @@ public class SEBRAAnonymizer {
         if (args.length != 3) {
             System.out.println("Usage arguments: <path to source CSV> <path to destination CSV> <path to names mapping CSV>");
         }
+        Path saltFile = Path.of("salt.txt");
+        if (!Files.exists(saltFile)) {
+            Files.createFile(saltFile);
+        }
+        String salt = Files.readString(saltFile);
+        if (StringUtils.isBlank(salt)) {
+            salt = RandomStringUtils.randomAlphanumeric(300);
+            Files.writeString(saltFile, salt);
+        }
+        
         Map<String, List<Organization>> organizations = new HashMap<>();
         try (Reader reader = new InputStreamReader(new BufferedInputStream(new FileInputStream(args[2])), "cp1251")) {
             CSVParser parser = CSVParser.parse(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader());
@@ -67,11 +102,11 @@ public class SEBRAAnonymizer {
                 } else {
                     org.setName(name);
                 }
-                org.setFrom(LocalDate.parse(record.get(3).trim().split("\\s+")[0], DATE_FORMAT).atStartOfDay());
+                org.setFrom(LocalDate.parse(record.get(3).trim().split("\\s+")[0], DATE_FORMAT).atStartOfDay().minusDays(1));
                 if (record.get(4).endsWith("3333")) {
                     org.setTo(LocalDateTime.now().plusWeeks(1));
                 } else {
-                    org.setTo(LocalDate.parse(record.get(4), DATE_FORMAT).atStartOfDay());
+                    org.setTo(LocalDate.parse(record.get(4), DATE_FORMAT).atStartOfDay().plusDays(1));
                 }
                 list.add(org);
             }
@@ -130,6 +165,8 @@ public class SEBRAAnonymizer {
                         if (header) {
                             row.add("ORGANIZATION");
                             row.add("PRIMARY_ORGANIZATION");
+                            row.add("PRIMARY_ORG_CODE");
+                            row.add("CLIENT_NAME_HASH");
                         } else {
                             List<Organization> orgs = organizations.get(record.get(4));
                             if (orgs != null) {
@@ -139,8 +176,7 @@ public class SEBRAAnonymizer {
                                 } else {
                                     boolean foundOrganization = false;
                                     for (Organization org : orgs) {
-                                        // +1 to avoid missing transactions on the day of change
-                                        LocalDateTime txDate = LocalDate.parse(record.get(0), DATE_FORMAT).atStartOfDay().plusDays(1);
+                                        LocalDateTime txDate = LocalDate.parse(record.get(0), DATE_FORMAT).atStartOfDay();
                                         if (txDate.isAfter(org.getFrom()) && txDate.isBefore(org.getTo())) {
                                             row.add(org.getName());
                                             foundOrganization = true;
@@ -153,12 +189,28 @@ public class SEBRAAnonymizer {
                                 }
                             }
                             // add primary organization
-                            List<Organization> primaryOrgs = organizations.get(record.get(4).substring(0, 3) + StringUtils.repeat("*", 7));
+                            String primaryOrganizationCode = record.get(4).substring(0, 3);
+                            List<Organization> primaryOrgs = organizations.get(primaryOrganizationCode + StringUtils.repeat("*", 7));
                             if (primaryOrgs != null) {
-                                row.add(primaryOrgs.get(primaryOrgs.size() - 1).getName());
+                                boolean foundOrganization = false;
+                                for (Organization org : primaryOrgs) {
+                                    LocalDateTime txDate = LocalDate.parse(record.get(0), DATE_FORMAT).atStartOfDay();
+                                    if (txDate.isAfter(org.getFrom()) && txDate.isBefore(org.getTo())) {
+                                        row.add(org.getName());
+                                        foundOrganization = true;
+                                        break;
+                                    }
+                                }
+                                if (!foundOrganization) {
+                                    row.add(primaryOrgs.get(primaryOrgs.size() - 1).getName());
+                                }
                             } else {
                                 row.add("");
                             }
+                            row.add(primaryOrganizationCode);
+                            
+                            // hash a salt + normalized, untransliterated, lowercase beneficary name
+                            row.add(bytesToHex(DIGEST.digest((salt + beneficiary.replaceAll("\\s+", " ")).getBytes(StandardCharsets.UTF_8))));
                         }
                         printer.printRecord(row);
                         header = false;
@@ -204,7 +256,18 @@ public class SEBRAAnonymizer {
     
     private static boolean isPersonalName(String str) {
         String lowerCaseString = str.toLowerCase().trim();
-        return WHITESPACE.matcher(lowerCaseString).results().count() == 2 && 
+        long wordCount = WHITESPACE.matcher(lowerCaseString).results().count();
+        // 4 word names are the exception, so we have to filter out beneficiaries with short words in them
+        if (wordCount == 3) {
+            String[] parts = lowerCaseString.split("\\s+");
+            for (String part : parts) {
+                if (part.length() <= 3) {
+                    return false;
+                }
+            }
+        }
+        
+        return (wordCount == 2 || wordCount == 3) && 
                 (NAME_SUFFIXES.stream().anyMatch(s -> lowerCaseString.matches(".+\\p{L}{2,}" + s + "\\b.+") || 
                         OTHER_PERSONAL_NAMES.stream().anyMatch(name -> lowerCaseString.contains(name))));
     }
@@ -214,6 +277,18 @@ public class SEBRAAnonymizer {
         return result;
     }
 
+    private static String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder(2 * hash.length);
+        for (int i = 0; i < hash.length; i++) {
+            String hex = Integer.toHexString(0xff & hash[i]);
+            if(hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+    
     public static class Organization {
         private String code;
         private LocalDateTime from;
